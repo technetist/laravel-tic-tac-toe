5b1f74fd45eaa88369383265dabb2a29f378f70d
Adrien Maranville <adrien.maranville@gmail.com>
2017-05-06 17:51:38 -0600

    created migrations to update user with score column and create tables for games and turns

---
+++ database/migrations/2017_05_06_225353_create_games_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('winner_id')->unsigned()->nullable();
+            $table->timestamp('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
